ccede4e619917c862b3549b336b9c2b064ba314d
kieuloan.ntkl@gmail.com <kieuloan.ntkl@gmail.com>
2022-10-18 17:50:42 +0700

    [Initialized project]

---
+++ index.js
@@ -0,0 +1,55 @@
+let myName = "loan nguyen";
+console.log(myName);
+myName = "1997";
+console.log(myName);
+const PI = 3.14;
+console.log(PI);
+let age;
+let fullName = ("nguyen thi kieu loan");
+console.log(fullName.split(" "));
+let str1 = " str1";
+let str2 = " str2";
+let result = `${str1} ${str2} hihi`;
+console.log(result);
+let myNumber1 = 2.78645;
+console.log(Math.round(myNumber1));
+console.log(myNumber1 * myNumber1);
+console.log(typeof (myNumber1));
+
+let a = 2;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a **= a);//binn phuong
+console.log(a ** 2);//binh phuong
+console.log(Math.pow(a, 2)); //binh phuong
+let a1 = 2;
+let a2 = "3";
+console.log(a1 + a2);
+
+//bt2
+// let d="1";
+// let Pi=3.14;
+// console.log(d*180/Pi);
+
+//bt3
+// let c=10;
+// console.log(c*1.8+32);
+
+//bt4
+// let p=20;
+// let f=0.45359237;
+// console.log(p*0.45359237);
+
+
+//bt5
+let n = 87;
+let n1 = n - 1;
+console.log(6 * n - f);
+
+
+
+
